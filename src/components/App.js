e9acdceeedaa9e13b17e00cf832a221948106f60
Dola Sabar <dgobind285@gmail.com>
2025-03-03 15:15:59 +0530

    listning function

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Microphone } from "@mynaui/icons-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
@@ -12,6 +14,21 @@ import {
 import { Button } from "@/components/ui/button";
 
 const App = () => {
+  const [text, setText] = useState("hello world");
+
+  function startRecording() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+
+      recognition.onresult = async function(event){
+        const transcript = event.results[0][0].transcript;
+        setText(transcript);
+      }
+
+      recognition.start();
+  }
+
   return (
     <div className="w-full max-w-md text-center ">
       <Card>
@@ -20,13 +37,7 @@ const App = () => {
         </CardHeader>
         <CardContent>
           <ScrollArea className="h-[200px] rounded-md border p-4">
-            Jokester began sneaking into the castle in the middle of the night
-            and leaving jokes all over the place: under the king's pillow, in
-            his soup, even in the royal toilet. The king was furious, but he
-            couldn't seem to stop Jokester. And then, one day, the people of the
-            kingdom discovered that the jokes left by Jokester were so funny
-            that they couldn't help but laugh. And once they started laughing,
-            they couldn't stop.
+            <div>{text}</div>
           </ScrollArea>
         </CardContent>
         <CardFooter>
